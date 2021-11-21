1b4a19b47151b1ebefb52998dd60c2e493fce872
aimakov <nurbolat.aimakov@gmail.com>
2021-11-21 22:15:58 +0900

    Added winning lines

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Game.css";
 
 const GameCell = (props) => {
     const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        if (props.checkedList.length === 0) setChecked(false);
+    }, [props.checkedList]);
 
     const checkingCell = () => {
         if (!checked) {
@@ -32,17 +36,36 @@ const GameCell = (props) => {
 
 const Game = () => {
     const [checkedList, setCheckedList] = useState([]);
+    const [winningSequence, setWinningSequence] = useState([]);
+    const [winClass, setWinClass] = useState();
 
     useEffect(() => {
         if (checkedList.length > 0) {
             for (let i = 0; i < checkedList.length / 2; i++) {
-                if (checkedList[i] % 3 === 0 && checkedList.includes(checkedList[i] + 1) && checkedList.includes(checkedList[i] + 2)) console.log("Won!");
-                if (checkedList.includes(checkedList[i] + 3) && checkedList.includes(checkedList[i] + 6)) console.log("Won!");
-                if (checkedList[i] === 2 && checkedList.includes(checkedList[i] + 2) && checkedList.includes(checkedList[i] + 4)) console.log("Won!");
-                if (checkedList.includes(checkedList[i] + 4) && checkedList.includes(checkedList[i] + 8)) console.log("Won!");
+                if (checkedList[i] % 3 === 0 && checkedList.includes(checkedList[i] + 1) && checkedList.includes(checkedList[i] + 2))
+                    setWinningSequence([checkedList[i], checkedList[i] + 1, checkedList[i] + 2]);
+                if (checkedList.includes(checkedList[i] + 3) && checkedList.includes(checkedList[i] + 6))
+                    setWinningSequence([checkedList[i], checkedList[i] + 3, checkedList[i] + 6]);
+                if (checkedList[i] === 2 && checkedList.includes(checkedList[i] + 2) && checkedList.includes(checkedList[i] + 4))
+                    setWinningSequence([checkedList[i], checkedList[i] + 2, checkedList[i] + 4]);
+                if (checkedList.includes(checkedList[i] + 4) && checkedList.includes(checkedList[i] + 8))
+                    setWinningSequence([checkedList[i], checkedList[i] + 4, checkedList[i] + 8]);
             }
         }
     }, [checkedList]);
+
+    useEffect(() => {
+        console.log(winningSequence);
+
+        if (winningSequence[0] === 0 && winningSequence[2] === 2) setWinClass("firstHor");
+        if (winningSequence[0] === 0 && winningSequence[2] === 8) setWinClass("firstDia");
+        if (winningSequence[0] === 3 && winningSequence[2] === 5) setWinClass("secondHor");
+        if (winningSequence[0] === 6 && winningSequence[2] === 8) setWinClass("thirdHor");
+        if (winningSequence[0] === 0 && winningSequence[2] === 6) setWinClass("firstVert");
+        if (winningSequence[0] === 1 && winningSequence[2] === 7) setWinClass("secondVert");
+        if (winningSequence[0] === 2 && winningSequence[2] === 8) setWinClass("thirdVert");
+        if (winningSequence[0] === 2 && winningSequence[2] === 6) setWinClass("secondDia");
+    }, [winningSequence]);
 
     return (
         <>
@@ -51,6 +74,8 @@ const Game = () => {
                     {[...Array(9).keys()].map((i) => (
                         <GameCell key={i} keyProp={i} setCheckedList={setCheckedList} checkedList={checkedList} />
                     ))}
+                    <button onClick={() => (setCheckedList([]), setWinningSequence([]))}>Clear</button>
+                    {winningSequence.length > 0 ? <div className={`WinningLine ${winClass}`} /> : null}
                 </div>
             </div>
         </>
